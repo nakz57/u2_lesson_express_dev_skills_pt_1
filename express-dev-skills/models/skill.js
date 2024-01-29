027c407af5394298328321ed5a5f3949efc6b2f3
Nayef Alzayani <nzayani57@outlook.com>
2024-01-29 16:19:20 +0300

    model done

---
+++ express-dev-skills/models/skill.js
@@ -0,0 +1,23 @@
+const skills = [
+  { id: 100, skill: 'Programming', done: true },
+  { id: 200, skill: 'Boxing', done: true },
+  { id: 300, skill: 'Professional Energy', done: true },
+  { id: 400, skill: 'Millionaire', done: false }
+]
+
+const getAll = () => {
+  return skills
+}
+
+const getOne = (id) => {
+  let skill = skills.find((skills) => {
+    //find todo where the id is equal to the id parameter
+    return skill.id === parseInt(id)
+  })
+  return skill
+}
+
+module.exports = {
+  getAll,
+  getOne
+}
